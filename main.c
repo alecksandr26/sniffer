c0ea2ed1e1a0529f26d53ccf51d0e63cc73f3572
Erick <erick.carrillo4982@alumnos.udg.mx>
2022-04-27 08:11:10 -0500

    A new interface of the binary

--- main.c
+++ main.c
@@ -7,19 +7,31 @@
 #include "include/helpers/file.h"
 #include "include/helpers/network.h"
 
+
+
+void usage (char *main)
+{
+    fprintf(stderr, "Error: wrong arguments\n\n");
+    fprintf(stderr, "To read a file\n\n");
+    fprintf(stderr, "\tUsage: %s --file <file-name>\n\n", main);
+    fprintf(stderr, "\tOr you can use\n\n");
+    fprintf(stderr, "\tUsage: %s -f <file-name>\n\n", main);
+    fprintf(stderr, "To read a package from the network\n\n");
+    fprintf(stderr, "\tUsage: %s --network\n\n", main);
+    fprintf(stderr, "\tOr you can use\n\n");
+    fprintf(stderr, "\tUsage: %s -n\n\n", main);
+    exit(EXIT_FAILURE);
+}
+
+
 int main (int argc, char *argv[])
 {
     char ans[100];
     int i;
     
-	if (3 != argc && argc != 2) {
-		fprintf(stderr, "Error: wrong arguments\n");
-        fprintf(stderr, "To read a file\n");
-		fprintf(stderr, "\tUsage: %s file <file-name>\n", argv[0]);
-        fprintf(stderr, "To read a package from the network\n");
-        fprintf(stderr, "\tUsage: %s network\n", argv[0]);
-		exit(EXIT_FAILURE);
-	} else if ((strcmp(argv[1], "--file") == 0) || (strcmp(argv[1], "-f") == 0)) { /* To read a file */
+	if (3 != argc && argc != 2)
+		usage(argv[0]);
+	else if ((strcmp(argv[1], "--file") == 0) || (strcmp(argv[1], "-f") == 0)) { /* To read a file */
         F *f = File(argv[1]);
         Pak *pack = Package(f->data, f->length);
         pack->print(pack);
@@ -42,13 +54,8 @@ int main (int argc, char *argv[])
             for (i = 0; ans[i] != 0; ++i)
                 ans[i] = tolower(ans[i]);
         } while (strcmp("yes", ans) == 0);
-    } else {
-        fprintf(stderr, "%s: Error: wrong arguments\n", argv[0]);
-        fprintf(stderr, "To read a file\n");
-		fprintf(stderr, "\tUsage: %s file <file-name>\n", argv[0]);
-        fprintf(stderr, "To read a package from the network\n");
-        fprintf(stderr, "\tUsage: %s network\n", argv[0]);
-		exit(EXIT_FAILURE);
-    }
+    } else
+        usage(argv[0]);
+
     return 0;
 }
